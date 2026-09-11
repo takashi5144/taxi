30c53c30c5a3ed5fbfeeeeaf9d663a4de54304b8
takashi5144 <kakatata825@gmail.com>
2026-09-11 09:56:35 +0900

    カレンダー日付クリックで始業・終業を入力できるようにした

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker�E�オフラインキャチE��ュ対応！E
 // アプリはindex.html単体で動作するため、キャチE��ュ対象は最小限に絞る
-const CACHE_NAME = 'taxi-support-v3.88.0-home-sales';
+const CACHE_NAME = 'taxi-support-v3.89.0-cal-shift';
 const STATIC_ASSETS = [
   './',
   './index.html',
