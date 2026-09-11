00cd32cff98adf4b7006aa67687329299389b52e
takashi5144 <kakatata825@gmail.com>
2026-09-11 10:11:27 +0900

    日付クリック詳細は始業・終業の保存まで残し売上合計以降を削除

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker�E�オフラインキャチE��ュ対応！E
 // アプリはindex.html単体で動作するため、キャチE��ュ対象は最小限に絞る
-const CACHE_NAME = 'taxi-support-v3.92.0-month-net';
+const CACHE_NAME = 'taxi-support-v3.93.0-day-shift-only';
 const STATIC_ASSETS = [
   './',
   './index.html',
